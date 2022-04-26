e9d9610abcae5900b774cd69601c6f67ec3e2a63
TakayumAja <asraftakayuma76@gmail.com>
2022-04-26 14:23:24 +0700

    rename file Header.js to index.js

--- src/component/Home/index.js
+++ src/component/Home/index.js
@@ -3,7 +3,7 @@ import React from "react";
 // Import Component
 import PassengerInput from "../PassengerInput";
 import ListPassenger from "../ListPassenger";
-import Header from "../Header/Header";
+import Header from "../Header";
 
 // Import File Css
 import "./Home.module.css";
